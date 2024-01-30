f3be2fe654575303d6fa75fb402d94438f410a30
jasonharriot <157506217+jasonharriot@users.noreply.github.com>
2024-01-30 13:29:45 -0800

    Update SerialNode.ino

--- SerialNode/SerialNode.ino
+++ SerialNode/SerialNode.ino
@@ -3,34 +3,58 @@
 
 #include <Wire.h>
 #include "DAQSensor.h"
+#include "DAQActuator.h"
+
+#define PUMPRELAYPIN 2
+#define PUMPPWMPIN 9
 
 Sensor sensor0(9, 0);
+
 AnalogSensor sensor1(A0, 1);
 DigitalSensor sensor2(12, 2, true);
 
+
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Copyright EDAC Labs 2024");
-  Serial.println("Data Acquisition Serial Node");
+  //Serial.println("Copyright EDAC Labs 2024");
+  //Serial.println("Data Acquisition Serial Node");
+  //Serial.println("ID, Type, Value");
+
+  pinMode(PUMPRELAYPIN, OUTPUT);
+  pinMode(PUMPPWMPIN, OUTPUT);
+
+  analogWrite(PUMPPWMPIN, 127);
 
   sensor0.init();
   sensor1.init();
   sensor2.init();
 
 
-  sensor1.summary();
-  sensor2.summary();
+  //sensor1.summary();
+  //sensor2.summary();
+
+  digitalWrite(PUMPRELAYPIN, 1);
+
+  
 }
 
-void loop() {
-  
-  
+void loop() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
   sensor1.read();
   //sensor1.deliver();
+  Serial.println(sensor1.value);
 
   sensor2.read();
   //sensor2.deliver();
-  sensor2.summary();
+  //sensor2.summary();
 
-  delay(1000);
+  /*if(sensor2.value){
+    digitalWrite(PUMPRELAYPIN, 0);
+  } else{
+    digitalWrite(PUMPRELAYPIN, 1);
+  }*/
+
+  
+
+  delay(100);
 }
